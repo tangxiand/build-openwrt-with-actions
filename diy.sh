7b3d9d9dc224c3df57d2fd798861fb00be339084
tangxiand <197554977@qq.com>
2026-03-02 22:27:12 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,20 +1,24 @@
 # 请在下方输入自定义命令(一般用来安装第三方插件)(可以留空)
 
 # 编辑默认的lan口ip地址
-sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+#!/bin/bash
+# diy-part2.sh - 补充iStore源配置
+set -e
 
-# 编辑默认的主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 添加iStore官方源
+if ! grep -q "istore" feeds.conf.default; then
+    echo 'src-git istore https://github.com/linkease/istore.git;main' >> feeds.conf.default
+    echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+    echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+fi
 
-# 编辑默认的luci显示的固件名称
-#sed -i 's/OpenWrt/ZWRT/g' package/base-files/files/bin/config_generate
-#sed -i 's/ImmortalWrt/ZWRT/g' package/base-files/files/bin/config_generate
+# 更新并安装feeds
+./scripts/feeds update -a
+./scripts/feeds install -a
 
-# 添加额外的软件包，echo 方式和git clone 方式二选一即可
-#echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git UA3F https://github.com/SunBK201/UA3F.git' >>feeds.conf.default
-#git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-#git clone https://github.com/kenzok8/small.git package/small
-#git clone https://github.com/SunBK201/UA3F.git package/UA3F
-git clone https://github.com/stevenjoezhang/luci-app-adguardhome.git package/ADGH
+# 应用配置
+make defconfig
+
+# 打印确认信息
+echo "✅ iStore源配置完成"
+echo "✅ 已安装所有feeds包"
